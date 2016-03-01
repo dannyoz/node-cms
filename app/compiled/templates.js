20bc848d01a63d0ae2730f86a3be29772a72fb24
Dan Osborne <danosborne@rocketmail.com>
2016-03-01 12:28:23 +0000

    rebuilt template cache

--- app/compiled/templates.js
+++ app/compiled/templates.js
@@ -4,9 +4,9 @@
 
 angular.module('app').run(['$templateCache', function($templateCache) {
 
-  $templateCache.put('components/footer/footer.tmpl.html', '<div class="container">footer<ul><li><a href="#" class="item1">Lorem ipsum.</a></li><li><a href="#" class="item2">Perferendis, accusantium.</a></li><li><a href="#" class="item3">Unde, quam.</a></li><li><a href="#" class="item4">Quos, recusandae.</a></li><li><a href="#" class="item5">Dolore, corrupti.</a></li></ul></div>');
-
   $templateCache.put('components/header/header.tmpl.html', '<section id="main" class="container"><h1 ng-bind="::app.name"></h1></section>');
+
+  $templateCache.put('components/footer/footer.tmpl.html', '<div class="container">footer<ul><li><a href="#" class="item1">Lorem ipsum.</a></li><li><a href="#" class="item2">Perferendis, accusantium.</a></li><li><a href="#" class="item3">Unde, quam.</a></li><li><a href="#" class="item4">Quos, recusandae.</a></li><li><a href="#" class="item5">Dolore, corrupti.</a></li></ul></div>');
 
   $templateCache.put('views/home/home.tmpl.html', '<section id="home-page" class="container"><h1 ng-bind="::homeText"></h1><h3 ng-bind="::strapline"></h3></section>');
 
--- cms-build/js/main.js
+++ cms-build/js/main.js
@@ -30120,9 +30120,9 @@ module.exports = angular;
 
 angular.module('cms').run(['$templateCache', function($templateCache) {
 
-  $templateCache.put('components/header/header.tmpl.html', '<section id="main" class="container"><h1 ng-bind="::app.name"></h1></section>');
-
   $templateCache.put('components/footer/footer.tmpl.html', '<div class="container">footer<ul><li><a href="#" class="item1">Lorem ipsum.</a></li><li><a href="#" class="item2">Perferendis, accusantium.</a></li><li><a href="#" class="item3">Unde, quam.</a></li><li><a href="#" class="item4">Quos, recusandae.</a></li><li><a href="#" class="item5">Dolore, corrupti.</a></li></ul></div>');
+
+  $templateCache.put('components/header/header.tmpl.html', '<section id="main" class="container"><h1 ng-bind="::app.name"></h1></section>');
 
   $templateCache.put('views/edit/edit.tmpl.html', '<section id="main-editor"><div id="header"></div><div id="sidebar"></div><div id="screen"><div class="padded-20"><form class="lined" ng-submit="update(textdata)"><input type="text" placeholder="text" ng-model="textdata"> <button class="primary">Add</button></form></div></div></section>');
 
